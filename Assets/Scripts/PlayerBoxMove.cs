7df7f41ae9f28266c7c68015cf5dc3c2682d74c8
BAZENTEI <mqcw@163.com>
2019-12-10 12:02:54 +0900

    jumpのブラシュアップ
    jumpの導入と伴うdebug

--- Assets/Scripts/PlayerBoxMove.cs
+++ Assets/Scripts/PlayerBoxMove.cs
@@ -13,14 +13,14 @@ public class PlayerBoxMove : MonoBehaviour {
 	//boxの変数
 	private GameObject boxTem;
     //Rigidbody2D RigidTem;
-
+    bool collected = false; //1210
     // Use this for initialization
     void Start()
     {
         root = transform.root.gameObject;
         defaultHandPos = transform.localPosition;
         //charcter_size = root.gameObject.GetComponent<RectTransform>().sizeDelta;
-		//magic number??
+		
 		HandUp = new Vector2( 0.0f,  4.5f);//修正　頭の上
 
 		//boxTem = GameObject.Find("Box (1)");
@@ -47,63 +47,67 @@ public class PlayerBoxMove : MonoBehaviour {
                 //boxに加える力を無視しないようにする
                 boxTem.GetComponent<Rigidbody2D> ().isKinematic = false;
                 boxTem = null;
-				//Debug.Log ("1");			
+				Debug.Log ("1");			
             }
   
         }
-        
+        collected = false;//1210
     }
 
     public void OnTriggerStay2D(Collider2D other)
     {
-        //Debug.Log("Hit");
-		//ジャンプ中は持ち上げることができない
-		if (other.gameObject.tag == "boxTrigger") {
+        if (!collected)
+        {
+            //ジャンプ中は持ち上げることができない
+            if (other.gameObject.tag == "boxTrigger")
+            {
+                // Debug.Log(Time.deltaTime);
+                //if (this.root.GetComponent<PlayerController> ().inair == false) 
+                {
+                    if (Input.GetKeyDown(KeyCode.X) && catch_box == false)
+                    {
+                        transform.localPosition = HandUp;
 
-			if (this.root.GetComponent<PlayerController> ().inair == false) {
-				if (Input.GetKeyDown(KeyCode.X) && catch_box == false)
-				{
-					transform.localPosition = HandUp;
-					 
-					catch_box = true;
+                        catch_box = true;
 
-					GameObject box = other.transform.root.gameObject;
+                        GameObject box = other.transform.root.gameObject;
 
-					box.transform.parent = transform;
+                        box.transform.parent = transform;
 
-					box.transform.position = transform.position;
+                        box.transform.position = transform.position;
 
 
-					boxTem = box.gameObject;
-					//LPBody更新
-		
-					//Debug.Log("key down");
-					//boxに加える力を無視するようにする
-					box.GetComponent<Rigidbody2D> ().isKinematic = true;
-                    //box.GetComponent<Rigidbody2D> ().Sleep();//= false;
-                    //this.root.GetComponent<PlayerController>().cs = box.GetComponent<BoxCollider2D>();
+                        boxTem = box.gameObject;
+                        //LPBody更新
 
-                   // Instantiate(box.GetComponent<Rigidbody2D>()) ;
+                        //Debug.Log("key down");
+                        //boxに加える力を無視するようにする
+                        box.GetComponent<Rigidbody2D>().isKinematic = true;
+                        box.GetComponent<Rigidbody2D>().useFullKinematicContacts = true;
+                        //box.GetComponent<Rigidbody2D> ().Sleep();//= false;
+                        //this.root.GetComponent<PlayerController>().cs = box.GetComponent<BoxCollider2D>();
 
-                 // Destroy(box.GetComponent<Rigidbody2D>());
+                        // Instantiate(box.GetComponent<Rigidbody2D>()) ;
+
+                        // Destroy(box.GetComponent<Rigidbody2D>());
+                        Debug.Log("Key Up 1");
+                        collected = true;   //1210
+                    }
+                    else if (Input.GetKeyDown(KeyCode.X) && catch_box == true)
+                    {
+                        catch_box = false;
+                        Debug.Log("Key Up2");
+                        collected = true;//1210
+
+                    }
+
 
                 }
-				else if (Input.GetKeyDown(KeyCode.X) && catch_box == true)
-				{
-					catch_box = false;
-					Debug.Log("Key Up");    
-				}
 
 
-			}
+            }
 
+        }
 
-
-
-
-
-			
-		}
-			
     }
 }
