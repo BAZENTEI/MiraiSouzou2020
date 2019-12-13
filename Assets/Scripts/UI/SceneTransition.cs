d6881199fc45de9ac3432d069f9bf53d347d7768
chichengjerry <chichengjerry@gmail.com>
2019-12-13 10:47:56 +0900

    UI Update

---
+++ Assets/Scripts/UI/SceneTransition.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+
+    public Animator transitionAnim;
+    public GameObject GameClearObject;
+
+    public string targetSceneName;
+
+    public static string NextSceneId { get; private set; }
+
+    void Start()
+    {
+        GameClearObject.SetActive(false);
+        string CurrentSceneId = SceneManager.GetActiveScene().name;
+        int id = 0;
+        try
+        {
+            id = int.Parse(CurrentSceneId.Replace("Scene", ""));
+        }
+        catch(System.Exception e)
+        {
+            Debug.Log("Exception source: " + e.Source);
+        }
+
+        NextSceneId = "Scene" + (id % 9 + 1);
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetButtonDown("Reset"))
+        {
+            StartCoroutine(GameRestart());
+        }
+        if (Input.GetButtonDown("Submit"))
+        {
+            StartCoroutine(GameClear());
+        }
+	}
+
+    IEnumerator GameRestart()
+    {
+        string CurrentSceneId = SceneManager.GetActiveScene().name;
+
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(CurrentSceneId);
+    }
+
+    IEnumerator GameClear()
+    {
+        transitionAnim.SetTrigger("end");
+        GameClearObject.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+
+        SceneManager.LoadScene(targetSceneName);
+    }
+}
