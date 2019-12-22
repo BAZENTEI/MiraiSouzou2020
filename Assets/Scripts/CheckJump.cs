a49755173087407002bd69b4c77db27b5b691331
chichengjerry <chichengjerry@gmail.com>
2019-12-22 22:37:46 +0900

    UI: Add Tips

---
+++ Assets/Scripts/CheckJump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckJump : MonoBehaviour {
+
+    public player player;
+    Color color;
+    static Color transparent = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+    Image image;
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+        color = image.color;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(player.controlEnabled && player.jumpState == player.JumpState.Grounded)
+        {
+            image.color = color;
+        }
+        else
+        {
+            image.color = transparent;
+        }
+	}
+}
---
+++ Assets/Scripts/UI/CheckAction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckAction : MonoBehaviour {
+
+    public player player;
+
+    public PlayerBoxMove playerHand;
+
+    Color color;
+    static Color transparent = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+    public enum CheckType
+    {
+        All,
+        Drop,
+        Pick,
+    }
+    public CheckType type;
+    Image image;
+
+	void Start () {
+        image = GetComponent<Image>();
+        color = image.color;
+	}
+
+    void SetOpacity(bool isOpacity)
+    {
+        image.color = isOpacity ? color : transparent;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch(type)
+        {
+            case CheckType.All:
+                SetOpacity(player.controlEnabled);
+                break;
+            case CheckType.Drop:
+                SetOpacity(player.controlEnabled && playerHand.catch_box);
+                break;
+            case CheckType.Pick:
+                SetOpacity(player.controlEnabled && !playerHand.catch_box);
+                break;
+        }
+	}
+}
